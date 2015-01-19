8f21a87da2c6966825827bc3fb2d07f1dc4a13bd
Hideki Saito <hideki@hidekisaito.com>
2015-01-19 11:59:29 -0800

    Downloading now uses curl

--- get.sh
+++ get.sh
@@ -5,11 +5,13 @@ source packages.list
 
 cd files
 
-gpg --keyserver hkp://keys.gnupg.net --recv-key 4f25e3b6
-gpg --keyserver hkp://keys.gnupg.net --recv-key e0856959
-gpg --keyserver hkp://keys.gnupg.net --recv-key 33bd3f06
+#gpg --keyserver hkp://keys.gnupg.net --recv-key 4f25e3b6
+#gpg --keyserver hkp://keys.gnupg.net --recv-key e0856959
+#gpg --keyserver hkp://keys.gnupg.net --recv-key 33bd3f06
 
 gpg_verify="gpg -d"
+downloader="curl -# -O"
+
 package_pinentry="pinentry-0.9.0"
 
 touch a.tar.bz2
@@ -19,10 +21,13 @@ rm *.tar.bz2
 rm *.tar.bz2.sig
 
 for ((i=0;i<${#package_list[@]};++i)); do
-        wget ftp://ftp.gnupg.org/gcrypt/${package_list[i]}/${package_list[i]}-${package_version[i]}.tar.bz2 ftp://ftp.gnupg.org/gcrypt/${package_list[i]}/${package_list[i]}-${package_version[i]}.tar.bz2.sig
+	echo "Downloading" ${package_list[i]}
+        $downloader ftp://ftp.gnupg.org/gcrypt/${package_list[i]}/${package_list[i]}-${package_version[i]}.tar.bz2 
+	$downloader ftp://ftp.gnupg.org/gcrypt/${package_list[i]}/${package_list[i]}-${package_version[i]}.tar.bz2.sig
         $gpg_verify ${package_list[i]}-${package_version[i]}.tar.bz2.sig
 done
 
-wget ftp://ftp.gnupg.org/gcrypt/pinentry/$package_pinentry.tar.bz2  ftp://ftp.gnupg.org/gcrypt/pinentry/$package_pinentry.tar.bz2.sig
+$downloader ftp://ftp.gnupg.org/gcrypt/pinentry/$package_pinentry.tar.bz2
+$downloader ftp://ftp.gnupg.org/gcrypt/pinentry/$package_pinentry.tar.bz2.sig
 $gpg_verify $package_pinentry.tar.bz2.sig
 cd ..
