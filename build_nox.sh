465cdc919f17bb0ac1013326afa040fc8d8007b7
Hideki Saito <hideki@hidekisaito.com>
2015-01-28 20:59:53 -0800

    "No X" variant for testing

---
+++ build_nox.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# (Order) libgpg-error, libksba, libgcrypt, libassuan, npth, gnupg
+
+set -e
+set -o pipefail
+
+tar_cmd="tar xvjf"
+gpg_verify="gpg -d"
+
+source packages.list
+
+cd files
+
+for ((i=0;i<${#package_list[@]};++i)); do
+	$gpg_verify ${package_list[i]}-${package_version[i]}.tar.bz2.sig
+	$tar_cmd ${package_list[i]}-${package_version[i]}.tar.bz2
+	cd ${package_list[i]}-${package_version[i]}
+	./configure $*
+	make
+	make install
+	cd ..
+	rm -r ${package_list[i]}-${package_version[i]}
+done
+
+
+# Pinentry package seems to require some patching
+# (At least on Ubuntu...)
+$gpg_verify $package_pinentry.tar.bz2.sig
+$tar_cmd $package_pinentry.tar.bz2
+cd $package_pinentry
+./configure $* --without-qt4 --without-gtk2 --enable-pinentry-curses
+cd qt4
+sed -i 's,../../../s/pinentry/qt4/,,g;' *.moc
+cd ..
+make
+make install
+cd ..
+rm -r $package_pinentry
+
+echo "Build complete."
+cd ..
