c514fb83078bf38c2f1af076e38285c71e93536f
Hideki Saito <hideki@hidekisaito.com>
2015-06-18 18:16:21 -0700

    Set permission for .gnupg when obtaining signatures.
    Resolves hsaito/gnupg-buildkit#3

--- updatesig.sh
+++ updatesig.sh
@@ -4,6 +4,8 @@ set -o pipefail
 
 gpg_command="gpg --keyserver hkp://keys.gnupg.net --homedir $PWD/.gnupg"
 
+chmod go-rwx $PWD/.gnupg
+
 $gpg_command --recv-key 249B39D24F25E3B6
 $gpg_command --recv-key 04376F3EE0856959
 $gpg_command --recv-key 2071B08A33BD3F06
